e77946d106584314e1d6c1a28b7b3f65079d25da
patrickfatrick <patrick.fricano@icloud.com>
2015-10-01 20:27:34 -0600

    making tests more universal

--- test/reform-restart-test.js
+++ test/reform-restart-test.js
@@ -4,12 +4,12 @@ var reform = require('../src/modules/reform');
 
 describe('reformRestart', function () {
  it('sets the date and time to the start of the specified increment', function () {
-	 reform('April 11, 1988 8:23:15.123 UTC').restart('s').d.toISOString().should.equal('1988-04-11T08:23:15.000Z');
-	 reform('April 11, 1988 8:23:15.123 UTC').restart('t').d.toISOString().should.equal('1988-04-11T08:23:00.000Z');
-	 reform('April 11, 1988 8:23:15.123 UTC').restart('h').d.toISOString().should.equal('1988-04-11T08:00:00.000Z');
-	 reform('April 11, 1988 8:23:15.123').restart('d').d.toISOString().should.equal('1988-04-11T06:00:00.000Z');
-	 reform('April 11, 1988 8:23:15.123').restart('w').d.toISOString().should.equal('1988-04-10T06:00:00.000Z');
-	 reform('April 11, 1988 8:23:15.123').restart('m').d.toISOString().should.equal('1988-04-01T07:00:00.000Z');
-	 reform('April 11, 1988 8:23:15.123').restart('y').d.toISOString().should.equal('1988-01-01T07:00:00.000Z');
+	 reform('April 11, 1988 8:23:15.123').restart('s').to('yyyy-mm-dd HH:tt:ss.ll').should.equal('1988-04-11 08:23:15.000');
+	 reform('April 11, 1988 8:23:15.123').restart('t').to('yyyy-mm-dd HH:tt:ss.ll').should.equal('1988-04-11 08:23:00.000');
+	 reform('April 11, 1988 8:23:15.123').restart('h').to('yyyy-mm-dd HH:tt:ss.ll').should.equal('1988-04-11 08:00:00.000');
+	 reform('April 11, 1988 8:23:15.123').restart('d').to('yyyy-mm-dd HH:tt:ss.ll').should.equal('1988-04-11 00:00:00.000');
+	 reform('April 11, 1988 8:23:15.123').restart('w').to('yyyy-mm-dd HH:tt:ss.ll').should.equal('1988-04-10 00:00:00.000');
+	 reform('April 11, 1988 8:23:15.123').restart('m').to('yyyy-mm-dd HH:tt:ss.ll').should.equal('1988-04-01 00:00:00.000');
+	 reform('April 11, 1988 8:23:15.123').restart('y').to('yyyy-mm-dd HH:tt:ss.ll').should.equal('1988-01-01 00:00:00.000');
  });
 });
