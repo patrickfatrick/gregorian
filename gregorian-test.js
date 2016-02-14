56b0e186c0b31464036a9b15c57b48cd11b474cd
patrickfatrick <patrick.fricano@icloud.com>
2016-02-14 14:35:49 -0700

    removed unnecessary file

--- gregorian-test.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-var gregorian = require('./src/gregorian')
-
-let manipulation = gregorian.reform().add(1, 'y').subtract(5, 'd').restart('m').to('D, M d, yyyy at hh:tt:ss.ll+AP')
-
-console.log(manipulation)
