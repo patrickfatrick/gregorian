32d8c8d97da8cb1fb25b9be68a35a3812e996b40
Patrick Fricano <patrick.fricano@icloud.com>
2023-01-12 15:09:14 -0800

    remove unused code in get

--- src/modules/get.js
+++ src/modules/get.js
@@ -1,35 +1,8 @@
 import _get from '../lib/_get';
 import { validateDate, curry } from '../lib/utils';
 
-export const getUTC = curry((increment, date) => {
-  date = date ?? new Date();
-  validateDate(date);
-  return _get(increment, date, 'UTC');
-});
-
 export const get = curry((increment, date) => {
   date = date ?? new Date();
   validateDate(date);
   return _get(increment, date);
-});
-
-export const getUTCFor = curry((increments, date) => {
-  date = date ?? new Date();
-  validateDate(date);
-  return Object.entries(increments).reduce(
-    (obj, [increment, v]) => ({
-      ...obj,
-      ...(v && { [increment]: _get(increment, date, 'UTC') }),
-    }),
-    {},
-  );
-});
-
-export const getFor = curry((increments, date) => {
-  date = date ?? new Date();
-  validateDate(date);
-  return Object.entries(increments).reduce(
-    (obj, [increment, v]) => ({ ...obj, ...(v && { [increment]: _get(increment, date) }) }),
-    {},
-  );
 });
